25fd59801e2a07594cad12fae26e859939acbfe0
jd <jdtan33@gmail.com>
2024-03-11 08:13:14 +0000

    create page for new plugin

---
+++ public_html/wp-content/plugins/pull-firebase/js/firebase-post.js
@@ -0,0 +1,8 @@
+(function ($) {
+  "use strict";
+  $(document).ready(function () {
+    $("#firebase-plugin-button").on("click", function () {
+      console.log("button pressed");
+    });
+  });
+})(jQuery);
